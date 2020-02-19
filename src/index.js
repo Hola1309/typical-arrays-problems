8449a687dae083ab36f2e1c6a556e31ddcf2078b
Hola <hola131098@gmail.com>
2020-02-19 22:33:31 +0400

    init: start Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+if (array == undefined || array == 0)
+	 {
+	 	return 0;
+	 }
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+	if (array == undefined || array == 0)
+	 {
+	 	return 0;
+	 }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+	if (array == undefined || array == 0)
+	 {
+	 	return 0;
+	 }
+	const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  return array.reduce(reducer)/array.length;
 }
